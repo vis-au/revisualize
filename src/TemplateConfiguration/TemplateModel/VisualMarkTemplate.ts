b1efbb1731fd9927d48852cc4f61a8d40653088b
Marius Hogräfer <mhograefer@cs.au.dk>
2019-03-25 17:01:36 +0100

    mod: visualmark -> visualmarktemplate

---
+++ src/TemplateConfiguration/TemplateModel/VisualMarkTemplate.ts
@@ -0,0 +1,9 @@
+import { Mark } from 'vega-lite/build/src/mark';
+
+import Template from './Template';
+
+export default class VisualMarkTemplate extends Template {
+  constructor(public type: Mark, parent: Template) {
+    super([], null, parent);
+  }
+}
