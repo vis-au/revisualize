0672e9319c5cc01402935a58637680b877316ef3
Marius Hogräfer <mhograefer@cs.au.dk>
2019-03-27 08:32:55 +0100

    add: resolve property to composite templates

--- src/TemplateConfiguration/TemplateModel/CompositionTemplate.ts
+++ src/TemplateConfiguration/TemplateModel/CompositionTemplate.ts
@@ -1,7 +1,10 @@
 import Template from "./Template";
 import { Composition } from "./LayoutType";
+import { Resolve } from "vega-lite/build/src/resolve";
 
 export default abstract class CompositionTemplate extends Template {
+  public resolve: Resolve;
+
   constructor(composition: Composition, visualElements: Template[], parent: Template = null) {
     super(visualElements, composition, parent);
   }
